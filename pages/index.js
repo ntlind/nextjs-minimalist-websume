1b8d7d2cc0150675c2761274ee3acb45a6ede10c
ntlind <>
2021-02-06 02:56:54 -0600

    finish for the night

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div>
       <SEO title="Fake" description="TODO" />
-      <div class="min-h-90 pt-10 lg:px-20 px-6 flex flex-col justify-center align-center">
+      <div class="min-h-90 pt-10 lg:px-40 px-6 flex flex-col justify-center align-center">
         <div item xs={12} className="text-title-responsive pb-10">
           Refining beauty <br />
           in visual identities
@@ -25,40 +25,26 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div class="min-h-90 pt-10 lg:px-20 px-6 flex flex-col justify-center align-center bg-black text-white">
-        <div class="text-5xl mb-20">Our services</div>
-        <Grid container class="flex flex-row justify-center align-center">
-          <Grid item lg={4}>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-              <img class="w-full" src="/mountain.jpg" alt="Mountain" />
-              <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">Mountain</div>
-                <p class="text-gray-700 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                  exercitationem praesentium nihil.
-                </p>
-              </div>
-              <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #photography
-                </span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #travel
-                </span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #winter
-                </span>
-              </div>
+      <div class="min-h-90 pt-10 lg:px-40 px-6 flex flex-col justify-center align-center bg-black text-white">
+        <div class="text-5xl mb-20 text-center lg:text-left">Our services</div>
+        <div class="grid grid-cols-1 xl:grid-cols-3 gap-6 xl:gap-12">
+          <div class="bg-gray rounded">
+            <div class="p-10 pb-10">
+              <div class="font-semibold text-xl mb-6">Machine Learning</div>
+              <p class="text-lightGrayText overflow-ellipsis">
+                Lorem ipsum dolor sit amet asd asd asd, consectetur adipisicing
+                elit. Voluptatibus quia, nulla!
+              </p>
             </div>
-          </Grid>
-          <Grid item lg={4}>
-            Service 2
-          </Grid>
-          <Grid item lg={4}>
-            Service 3
-          </Grid>
-        </Grid>
+            <div class="px-10 pb-10 grid grid-cols-3">
+              <span class="text-sm font-semibold text-gray-700">python</span>
+              <span class="text-sm font-semibold text-gray-700">scala</span>
+              <span class="text-sm font-semibold text-gray-700">lightgbm</span>
+            </div>
+          </div>{" "}
+          <div class="bg-gray card-tri">1</div>
+          <div class="bg-gray card-tri">1</div>
+        </div>
       </div>
     </div>
   );
