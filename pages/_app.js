5fe24c09e037a6f176160e4d04b45c9216d2c290
ntlind <>
2021-03-01 18:51:59 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
-import "tailwindcss/tailwind.css";
 
+import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
