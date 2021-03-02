0240ab7508070817788183a6bee298918665e46d
ntlind <>
2021-03-01 18:26:56 -0600

    fix viewport error from nextjs

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,10 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
+         
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="favicon.ico" />
           <link
